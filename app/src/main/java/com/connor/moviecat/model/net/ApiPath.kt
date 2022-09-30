9932232928f2e44dd27ef91f06f763c6ed190bd5
connor <wyh2542367414d@gmail.com>
2022-10-01 01:05:53 +0800

    commit

--- app/src/main/java/com/connor/moviecat/model/net/ApiPath.kt
+++ app/src/main/java/com/connor/moviecat/model/net/ApiPath.kt
@@ -3,7 +3,7 @@ package com.connor.moviecat.model.net
 object ApiPath {
 
     const val API_KEY = "api_key"
-    const val API_KEY_VALUE = "c28a0d55e4360c980ce424ad1d275770"
+    const val API_KEY_VALUE = ""
 
     const val TRENDING_ALL_WEEK = "trending/all/week"
 
